11a8805bb721c254e76859d45b2530d89fe53270
Caden <cadenrothzeid@gmail.com>
2022-08-24 06:37:15 -0600

    fixed bug so that page navigation works in development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://caden32.com',
-  integrations: [tailwind(), sitemap(), image()]
+  integrations: [tailwind(), sitemap(), image(), vue()]
 });
