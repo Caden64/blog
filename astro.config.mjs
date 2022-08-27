ac558af866dd6066fbabcca1a350d77c8d187f2c
Caden <cadenrothzeid@gmail.com>
2022-08-27 00:01:30 -0600

    code blocks can now be styled by prism js
    This is a huge deal because I can now change the color of the code that's displayed in markdown easier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
-
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://caden32.com',
-  integrations: [tailwind(), sitemap(), image(), vue()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
+    /*
     shikiConfig: {
-      theme: 'min-light',
-      langs: ['go'],
+      theme: 'github-light',
+      langs: ['go']
     }
+    */
+    syntaxHighlight: 'prism',
   }
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -52,10 +53,19 @@ module.exports = {
 					'800': '#9a0e88',
 					'900': '#7e116e',
 				},
+				"shiki-color-text": "var(--shiki-color-text)",
+				"shiki-color-background": "var(--shiki-color-background)",
+				"shiki-token-constant": "var(--shiki-token-constant)",
+				"shiki-token-string": "var(--shiki-token-string)",
+				"shiki-token-comment": "var(--shiki-token-comment)",
+				"shiki-token-keyword": "var(--shiki-token-keyword)",
+				"shiki-token-parameter": "var(--shiki-token-parameter)",
+				"shiki-token-function": "var(--shiki-token-function)",
+				"shiki-token-string-expression": "var(--shiki-token-string-expression)",
+				"shiki-token-punctuation": "var(--shiki-token-punctuation)",
+				"shiki-token-link": "var(--shiki-token-link)",
+
 			},
 		},
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
 }
