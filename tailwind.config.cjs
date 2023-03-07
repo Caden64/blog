0c4c5bfd7df7e87ffee4737fcb2746ac16dacab2
caden <caden@caden32.com>
2023-03-07 16:28:38 -0700

    more of pastel blue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,32 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-
+		extend: {
+			colors: {
+				'pastel-general': {
+					'light-pink': '#FFADAD',
+					'deep-champagne': '#FFD6A5',
+					'lemon-yellow-crayola': '#FDFFB6',
+					'tea-green': '#CAFFBF',
+					'electric-blue': '#9BF6FF',
+					'baby-blue-eyes': '#A0C4FF',
+					'Maximum-blue-purple': '#BDB2FF',
+					'Mauve': '#FFC6FF',
+					'dark-baby-blue-eyes': '#4a8cdb',
+					'border-baby-blue-eyes': '#8fc4ff',
+				},
+				'pastel-theme': {
+					'text-gray': '#27282a',
+					'gray': '#495159',
+					'teal': '#67CAC6',
+					'light-pink': '#D68589',
+					'off-white': '#F8F1DF',
+					'sweet-blue': '#4C8ED5',
+					'med-sweet-blue': '#6a9dd4',
+					'light-sweet-blue': '#ACCDEC',
+					'dark-sweet-blue': '#2668A6',
+				}
+			}
+		}
 	}
 }
