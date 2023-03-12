32bd508c9e86d864cd5e578224f83c0b4c440c53
caden <caden@caden32.com>
2023-03-11 18:43:07 -0700

    have full functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import preact from "@astrojs/preact";
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://caden32.com',
-  integrations: [tailwind(), sitemap(), preact(), svelte(), react()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     // syntaxHighlight: 'prism',
     extendDefaultPlugins: true
