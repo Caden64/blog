51fbe7afb42323bfcbd3f9a1dbad9bd8300e0ba9
Caden <cadenrothzeid@gmail.com>
2023-08-31 17:03:48 -0600

    clickable selectors

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,9 @@
-import {createSignal, For, Show} from "solid-js";
+import {Accessor, createSignal, For, Show} from "solid-js";
 
 export default function Counter(props: any) {
     const [count, setCount] = createSignal(1);
     const add = () => {
-        if (props.wow.length > count() + 2) {
+        if (props.amount > count() + 2) {
             setCount(count() + 1)
         }
     }
@@ -15,22 +15,29 @@ export default function Counter(props: any) {
 
     return (
         <>
-            <div class="flex items-center justify-center">
-                <Show when={count() > 1} fallback={<div class="px-1">*</div>}>
-                    <button onClick={subtract} class="px-1">-</button>
-                </Show>
-                <For each={props.wow} fallback={<div>loading...</div>}>
-                    {(item) => <div>
-                        <Show when={item.data.id >= count() && item.data.id < count() + 3}>
-                            <a href={`blog/${item.slug}`} class="px-4">
-                                {item.slug}
-                            </a>
-                        </Show>
-                    </div>}
-                </For>
-                <Show when={props.wow.length > count() + 2} fallback={<div class="px-1">*</div>}>
-                    <button onClick={add} class="px-1">+</button>
-                </Show>
+            <div class="flex items-center justify-center flex-col">
+                <div class="flex flex-row">
+                    <Show when={count() > 1} fallback={<div class="px-1 opacity-50">{`<`}</div>}>
+                        <button onClick={subtract} class="px-1"> {`<`} </button>
+                    </Show>
+                    <For each={props.blogs} fallback={<div>loading...</div>}>
+                        {(item) => <div>
+                            <Show when={item.data.id >= count() && item.data.id < count() + 3}>
+                                <a href={`blog/${item.slug}`} class="px-4">
+                                    {item.slug}
+                                </a>
+                            </Show>
+                        </div>}
+                    </For>
+                    <Show when={props.blogs.length > count() + 2} fallback={<div class="px-1 opacity-50">{`>`}</div>}>
+                        <button onClick={add} class="px-1">{`>`}</button>
+                    </Show>
+                </div>
+                <div class="flex">
+                    <For each={props.blogs.slice(1, props.blogs.length - 1)} fallback={<div>loading...</div>}>
+                        {(_, index) => <button class="m-0.5 p-1 px-6 bg-orange-500" onClick={() => {setCount(index() + 1) } } /> }
+                    </For>
+                </div>
             </div>
         </>
     )
