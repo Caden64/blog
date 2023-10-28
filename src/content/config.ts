4c2441554eff941f2f9213f9694463050c8a6dba
caden32 <caden@caden32.com>
2023-10-28 03:14:27 -0600

    improvements

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const blogCollection = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        tags: z.array(z.string()),
+        tags: z.array(z.string()).max(6),
         image: z.string().default("/favicon.png"),
         alt: z.string().default("This image does not have an alt. Sorry!"),
         id: z.number(),
