970d0f28399c8a850d9f28144957b26292e7b1f9
Caden <cadenrothzeid@gmail.com>
2023-12-30 12:59:03 -0700

    carousel on home page limited to 3 posts and better markdown formatting on the actual blog post pages

---
+++ src/content/blogs/post-4.md
@@ -0,0 +1,12 @@
+---
+title: Blog 4 
+description: This is a blog post.
+id: 4
+image:
+  url: /assets/logo.png
+  alt: Logo
+---
+
+# Test 4
+
+Why not there are already 3 blogs
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -28,5 +28,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+      require("@tailwindcss/typography"),
+      require("tailwindcss-animate")
+  ],
 }
